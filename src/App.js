4ac6ec0f70af0bfacb91c18a90e40a5b3065304d
2miinjeong <2miinjeong@gmail.com>
2024-01-30 17:41:46 +0900

    240130

--- src/App.js
+++ src/App.js
@@ -18,6 +18,25 @@ class App extends React.Component {
     this.state = {item:[{id:0, title:"Hello React", done:true},
     {id:1, title:"vue", done:false}, {id:2, title:"angular", done:false}]}
   }
+  //데이터를 추가하는 함수
+  add = (item) => {
+    //react는 prosp는 수정할 수 없고
+    //state는 수정이 가능하지만 원본을 직접 수정하는 것은 안되고
+    //setState 메서드를 이용해서만 수정이 가능
+
+    //배열에 데이터를 추가하기 위해서 state의 배열을 복사 
+    const thisItems = this.state.item;
+
+    //item의 id와 done값을 설정
+    item.id = "ID_" + thisItems.length;
+    item.done = false;
+
+    //복사한 데이터에 데이터를 추가
+    thisItems.push(item)
+    //복사한 데이터를 다시 state에 적용
+    this.setState({items:thisItems});
+  }
+
   render(){
     //배열을 순회하면서 출력할 내용을 생성
     //item은 배열을 순회할 때 각각의 데이터이고 idx는 인덱스 
@@ -36,7 +55,7 @@ class App extends React.Component {
     return(
       <div>
         <Container maxWidth="md">
-          <AddToDo />
+          <AddToDo add ={this.add}/>
           {display}
         </Container>
       </div>
